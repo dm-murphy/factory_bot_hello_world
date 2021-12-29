90435bffbabbec781bdee7d89c883ebea43d39c8
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-29 13:56:49 -0500

    Add nested factory for physician role in users.rb factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,9 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+
+    factory :physician_user do
+      role { 'physician' }
+    end
   end
 end
