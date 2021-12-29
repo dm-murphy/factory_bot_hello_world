1d01fd677894fe6dd6fc108f8b1885c9cc8c77b0
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-29 13:27:38 -0500

    Update users.rb spec to use Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    first_name { "John" }
-    last_name { "Smith" }
-    email { "john.smith@example.com" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
   end
 end
