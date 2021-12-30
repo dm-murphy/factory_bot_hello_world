2ebdf6c3ef4553b4c38ed7675d051f793a4136b0
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-12-30 15:17:24 -0500

    Add nested factory with callback for user with message

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,10 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     
-    trait :with_email do
-      email { "pppiff@example.com" }
-    end    
+    factory :user_with_message do
+      after(:create) do |user|
+        create(:message, user: user)
+      end
+    end
   end
 end
